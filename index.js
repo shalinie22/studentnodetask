9c4be06e3bc9749c208d819880461dd79021dd23
shalinie <shalushalinie22@gmail.com>
2020-07-10 14:58:19 +0530

    hhhhjbjh

--- index.js
+++ index.js
@@ -22,17 +22,30 @@ app.post("/staffdetails", (req, res) => {
 });
 
 app.get("/allstaff", (req, res) => {
-  let staf = staffdetails.map((data) => {
-    let count = studentdetails.filter((item) => item.staffid === data.id);
+  let staffdet = staffdetails.map((data) => {
+    let studentcount = studentdetails.filter((item) => item.staffid === data.id);
     return {
       id: data.id,
       name: data.name,
       email: data.email,
-      student_Count: count.length,
+      student_Count: studentcount.length,
     };
   });
-  res.json(staf);
+  res.json(staffdet);
 });
+
+app.delete("/studentdetails/:id", (req, res) => {
+ const id=req.params.id
+  let filterVal = studentdetails.filter((a) => {
+    if (a.id == id) {
+      return a;
+    }
+  })[0];
+  let index = studentdetails.indexOf(filterVal);
+  studentdetails.splice(index, 1);
+  res.send(studentdetails);
+});
+
 app.listen(process.env.PORT || port, () => {
   console.log(`server is listening ${port}`);
 });
